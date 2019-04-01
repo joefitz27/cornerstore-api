4b5906a5f718bfedaad26600a50467a2a7e0e597
jsauwse <46111395+jsauwse@users.noreply.github.com>
2019-03-31 18:49:32 -0700

    arrow functions updated

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-token, authorization");
   res.header("Access-Control-Expose-Headers", "x-token, authorization");
